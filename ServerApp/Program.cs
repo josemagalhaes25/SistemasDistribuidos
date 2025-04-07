8cde576bd462e2be83dc7e2a083b175ded4285f3
josemagalhaes25 <al77277@alunos.utad.pt>
2025-04-07 04:34:51 +0100

    Código inicial do ServerApp

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int port = 6000;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Servidor iniciado...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Thread thread = new Thread(() => HandleClient(client));
+            thread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[256];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Dado recebido do Agregador: " + message);
+
+            byte[] response = Encoding.UTF8.GetBytes("RECEIVED");
+            stream.Write(response, 0, response.Length);
+        }
+        client.Close();
+    }
+}
--- ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
+++ ServerApp/obj/Debug/net8.0/ServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188e178dc9eeb6367cf24ee25d61727ae00c7c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8331fe3502bb3e478f1ea03c236a82b1d88747")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
