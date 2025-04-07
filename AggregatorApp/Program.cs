f4e63c10c284c00ac6f6f504e9d96caeb48dbb28
josemagalhaes25 <al77277@alunos.utad.pt>
2025-04-07 05:12:54 +0100

    Teste2

---
+++ AggregatorApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int port = 5000;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Agregador iniciado...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Thread thread = new Thread(() => HandleClient(client));
+            thread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[256];
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Recebido do WAVY: " + message);
+
+                if (message.StartsWith("DATA"))
+                    ForwardToServer(message);
+                else if (message == "BYE")
+                {
+                    byte[] response = Encoding.UTF8.GetBytes("BYE_ACK");
+                    stream.Write(response, 0, response.Length);
+                    break;
+                }
+                else
+                {
+                    byte[] response = Encoding.UTF8.GetBytes("ACK");
+                    stream.Write(response, 0, response.Length);
+                }
+            }
+        }
+        client.Close();
+    }
+
+    static void ForwardToServer(string data)
+    {
+        using (TcpClient serverClient = new TcpClient("127.0.0.1", 6000))
+        using (NetworkStream stream = serverClient.GetStream())
+        {
+            byte[] message = Encoding.UTF8.GetBytes("FORWARD;" + data);
+            stream.Write(message, 0, message.Length);
+        }
+    }
+}
--- AggregatorApp/obj/Debug/net8.0/AggregatorApp.AssemblyInfo.cs
+++ AggregatorApp/obj/Debug/net8.0/AggregatorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AggregatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60555f9c697c713182adf3f6e127b6170e6d8b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ace3f31ddbfca879a15657bf5e9fd366e3ec262")]
 [assembly: System.Reflection.AssemblyProductAttribute("AggregatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AggregatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
